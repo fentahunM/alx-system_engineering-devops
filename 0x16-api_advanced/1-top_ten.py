8d4e031af685f52d29a6703ff72685a7990ba98e
fentahunM <fantahun360@gmial.com>
2024-05-07 22:49:26 +0300

    corrected

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,22 +1,21 @@
 #!/usr/bin/python3
-"""A function that queries the Reddit API and prints the titles of
-   the first 10 hot posts listed for a given subreddit.
-"""
-
-import requests
+""" function that queries a Reddit API """
+import requests as req
+import sys
 
 
 def top_ten(subreddit):
-    """prints the titles of the first 10 hot posts listed
-       for a given subreddit
-    """
-    try:
-        f = 'hot.json?limit=10'
-        url = 'https://www.reddit.com/r/{}/{}'.format(subreddit, f)
-        r = requests.get(url, headers={'User-agent': 'hello-student 0.1'})
-        d = r.json()
-        l = d.get('data').get('children')
-        for i in l:
-            print(i.get('data').get('title'))
-    except AttributeError:
+    """ Returns: top ten post titles """
+
+    headers = {'User-Agent': 'xica369'}
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    parameters = {'limit': 10}
+    response = req.get(url, headers=headers, allow_redirects=False,
+                       params=parameters)
+
+    if response.status_code == 200:
+        titles_ = response.json().get('data').get('children')
+        for title_ in titles_:
+            print(title_.get('data').get('title'))
+    else:
         print(None)
