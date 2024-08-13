2c3caff8b3089d5a6f7b964e12755986ce35b8af
fentahunM <fantahun360@gmail.com>
2024-08-13 20:07:25 +0300

    get total subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,10 +7,10 @@ import requests as req
 def number_of_subscribers(subreddit):
     """ this will return the number of subs of a given subreddit """
 
-    headers = {'User-Agent': 'xica369'}
+    headers = {'User-Agent': 'Mozilla/5.0 (compatible; Python script)'}
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     response = req.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
         return (response.json().get("data").get("subscribers"))
-    return (0)
+    return 0
